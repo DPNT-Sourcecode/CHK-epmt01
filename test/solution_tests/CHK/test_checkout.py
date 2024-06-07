6df2abc8e088e311dcc819ec822b66fd7e6e8fe1
sbx_user1051 <sbx_user1051@169.254.30.41>
2024-06-07 11:20:24 +0000

    Fri Jun 07 11:20:24 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,7 @@
-from lib.solutions.TST import one
+from lib.solutions.CHK import checkout_solution
 
 
-class TestSum():
-    def test_sum(self):
-        assert one.get() == 1
+class TestCheckout():
+    def test_checkout(self):
+        assert checkout_solution.checkout(skus='A, B, C, D') == 115
+
