b7f402acf0dc1d62bc4745ba429a36ddb6591c28
sbx_user1051 <sbx_user1051@169.254.30.41>
2024-06-07 11:38:24 +0000

    Fri Jun 07 11:38:24 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,23 +26,31 @@ def checkout(skus: str) -> int:
     """Returns an integer sum of the skus in the basket."""
     value = 0
 
-    sku_list = skus.split(",")
-    sku_list = [item.strip() for item in sku_list]
+    skus, value = special_items(skus=skus)
 
-
-
+    
     for sku in skus:
         if product := PRODUCTS.get(sku):
             value = value + product['price']
     return value
 
-def _special_items(skus: str) -> dict[str: int]:
+def special_items(skus: str) -> dict[str: int]:
     """Deals with the special items in a basket.
     
     Args:
         skus: string of skus in basket.
     Returns:
-        a dict containing a simplified basket with all special items 
+        A dict containing a simplified basket with all special items 
         accounted for and a value of the special items.
     
     """
+
+    sku_list = skus.split(",")
+    sku_list = [item.strip() for item in sku_list]
+
+    breakpoint()
+
+checkout(skus='A, B, C, D, D, D')
+
+
+
