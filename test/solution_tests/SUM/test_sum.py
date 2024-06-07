13906d6c04762759b6503452ea6cf4027b0436e7
sbx_user1051 <sbx_user1051@169.254.30.41>
2024-06-07 10:50:40 +0000

    Fri Jun 07 10:50:40 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,12 +3,15 @@ import pytest
 
 
 class TestSum():
-    @pytest.mark.parametrize("int_1, int_2, expected_result", [
-  (2, 3, 5),
-  (99, 98, 197),
-  (1, 1, 2)
-])
+    @pytest.mark.parametrize(
+        "int_1, int_2, expected_result", [
+            (2, 3, 5),
+            (99, 98, 197),
+            (1, 1, 2)
+        ]
+    )
     def test_sum(self, int_1, int_2, expected_result):
         assert sum_solution.compute(int_1, int_2) == expected_result
+
 
 
