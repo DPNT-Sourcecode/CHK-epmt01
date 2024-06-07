d9bd33291fffd9cf9d16d7b9a23343833cf0c055
sbx_user1051 <sbx_user1051@169.254.30.41>
2024-06-07 10:44:40 +0000

    Fri Jun 07 10:44:40 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def sum(int_a, int_b) -> int:
+def compute(int_a, int_b) -> int:
     """Returns the sum of 2 integer arguments, int_a and int_b."""
     return (int_a + int_b)
+
